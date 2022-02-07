9a0561798cbcebe52be1f0f6418ef8489293d185
satelightccb <crazy_soci80@yahoo.co.jp>
2022-02-07 23:39:10 +0900

    ファイルの追加

--- test.py
+++ test.py
@@ -6,17 +6,21 @@ def evacuate_data():
     insert_datas = []
     for data in datas:
         insert_data ={
-            "english_sentence":data.english_sentence,
-            "japanese_sentence":data.japanese_sentence,
-        }
+            "data.id":{
+                        "english_sentence":data.english_sentence,
+                        "japanese_sentence":data.japanese_sentence,
+                        }
+        } 
         insert_datas.append(insert_data)
 
     df = pd.DataFrame(insert_datas)
-    df.to_csv("english.csv", index=False, header=[
-            "english_sentence", "japanese_sentence"])
+    df.to_json("english.json",force_ascii=False)
 
 def restore_data():
-    df = pd.read_csv("english_csv")
-    english_table = English()
+    df = pd.read_json("english.json")
+    print(df["english_sentence"])
 
     # english_table.english_sentence = 
+
+# evacuate_data()
+restore_data()
